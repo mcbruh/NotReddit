fc91d449cc216bd8eb7da1f50411daad77842a0d
mcbruh <michael.chen.1120@gmail.com>
2017-05-24 17:15:21 -0400

    added links to layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
+	root to: 'posts#index'
+  	resources :comments
+  	resources :posts
   	devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
