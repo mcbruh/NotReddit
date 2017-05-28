3ad4fe566041379009aac7bf70cfc8508fdc6b9f
mcbruh <michael.chen.1120@gmail.com>
2017-05-27 22:24:17 -0400

    omniauth redirecting to sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,31 +1,36 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :omniauthable,
-		 :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :omniauthable, :omniauth_providers => [:twitter]
   has_many :posts
   has_many :comments
   has_many :votes, through: :posts
   has_many :votes, through: :comments
+  validates :email, presence: true, uniqueness:true
+  validates :username, presence: true, uniqueness: true
 
   def self.from_omniauth(auth)
-  	where(provider: auth.uid, uid: auth[:uid]).first_or_create do |user|
+  	where(provider: auth.uid, uid: auth[:uid]).first_or_create! do |user|
   		user.provider = auth.provider
-  		user.uid = auth.uid
   		user.username = auth.info.nickname
   		user.email = auth.info.email
-  		user.save
+      user.encrypted_password = Devise.friendly_token[0,20]
   	end
   end
 
   def self.new_with_session(params, session)
-  	if session["devise.user_attributes"]
-  		new(session["devise.user_attributes"], without_protection: true) do |user|
-  			user.attributes = params
-  			user.valid?
-  		end
-  	else
-  		super
-  	end
+    super.tap do |user|
+      if data = session["devise.twitter_data"]
+        user.username = data["nickname"]
+        user.email = data["email"]
+        user.uid = data[:uid]
+      end
+    end
   end
+
+  def password_required?
+  	super && provider.blank?
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20170526183534) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "provider"
-    t.integer "uid"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
