6a4772357e371121555434052158d7f61961eb30
mcbruh <michael.chen.1120@gmail.com>
2017-06-02 18:59:46 -0400

    basic comments model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
 	acts_as_commentable
 	belongs_to :user
-	has_many :comments, inverse_of: :post
+	has_many :comments, :as => :commentable
 	has_many :votes
-	accepts_nested_attributes_for :comments, reject_if: :all_blank, allow_destroy: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   	resources :posts do
   		resources :comments
   	end
-  	resources :comments
+  	resources :comments do
+  		resources :comments
+  	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
