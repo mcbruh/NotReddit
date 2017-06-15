e7b8f82019579ed36d2b39efcda47e74496e0c07
mcbruh <michael.chen.1120@gmail.com>
2017-06-15 18:25:56 -0400

    basic upvotes actions

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -2,7 +2,12 @@ class UpvotesController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
-
+		@upvote = Upvote.new
+		@upvote.user_id = current_user.id
+		@upvote.post_id = params[:post_id]
+		if @upvote.save
+			redirect_back fallback_location: root_path
+		end
 	end
 
 	def destroy
@@ -14,5 +19,5 @@ class UpvotesController < ApplicationController
 	def upvote_params
 		params.require(:upvotes).permit(:user_id, :post_id, :value)
 	end
-	
+
 end
