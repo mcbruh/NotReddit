4ca73fbfafac9a471985b69039e549a861fc9deb
mcbruh <michael.chen.1120@gmail.com>
2017-06-16 03:23:42 -0400

    rest of voting actions and models

--- app/models/comment_downvote.rb
+++ app/models/comment_downvote.rb
@@ -1,4 +1,14 @@
 class CommentDownvote < ApplicationRecord
 	belongs_to :user
 	belongs_to :comment
+
+	def self.destroy_comment_vote(user, comment)
+		current_comment_vote = Vote.where(user_id: user.id).where(comment_id: comment.id)
+		current_comment_vote.destroy if current_comment_vote
+	end
+
+	def self.delete_comment_vote(user, comment)
+		destroy_comment_vote(user, comment)
+	end
+
 end
