80979ce1acc498f095bfd8c4f1a10be40ffeb439
mcbruh <michael.chen.1120@gmail.com>
2017-06-16 20:28:24 -0400

    removing voting methods

--- app/helpers/comment_upvotes_helper.rb
+++
@@ -1,2 +0,0 @@
-module CommentUpvotesHelper
-end
--- test/controllers/comment_downvotes_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CommentDownvotesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
