625d2b1bc89010c6416654fcb8eceabf09a905b5
mcbruh <michael.chen.1120@gmail.com>
2017-06-17 07:15:27 -0400

    formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,25 +5,27 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :trackable, :omniauthable, :omniauth_providers => [:twitter]
   has_many :posts
   has_many :comments, through: :posts
+  has_many :votes, through: :posts
+  has_many :votes, through: :comments
   validates :email, presence: true, uniqueness:true
   validates :username, presence: true, uniqueness: true
   acts_as_voter
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user| 
-      user.username = auth.info.nickname
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-    end
+  	where(provider: auth.provider, uid: auth.uid).first_or_create do |user| 
+  		user.username = auth.info.nickname
+  		user.email = auth.info.email
+  		user.password = Devise.friendly_token[0,20]
+  	end
   end
 
   def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.twitter_data"] && session["devise.twitter_data"].except("extra")
-        user.username = data["nickname"] if user.username.blank?
-        user.email = data["email"] if user.email.blank?
-      end
-    end
+  	super.tap do |user|
+  		if data = session["devise.twitter_data"] && session["devise.twitter_data"].except("extra")
+  			user.username = data["nickname"] if user.username.blank?
+  			user.email = data["email"] if user.email.blank?
+  		end
+  	end
   end
 
 end
