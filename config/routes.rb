1936bbe490e44bdcdf9ac5e07c87e59e1fbc1273
mcbruh <michael.chen.1120@gmail.com>
2017-06-20 03:12:45 -0400

    add messages and conversations

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,10 @@ Rails.application.routes.draw do
 			put :downvote, to: 'comments#downvote'
 		end
 	end
+
+	namespace :admin do
+		resources :posts
+		resources :comments
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
