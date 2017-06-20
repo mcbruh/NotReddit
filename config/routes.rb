9770316d74f34d8e2f6900b8d30f9af8c2b49dca
mcbruh <michael.chen.1120@gmail.com>
2017-06-20 03:37:37 -0400

    conversations view

--- config/routes.rb
+++ config/routes.rb
@@ -26,5 +26,9 @@ Rails.application.routes.draw do
 		resources :posts
 		resources :comments
 	end
+
+	resources :conversations do
+		resources :messages
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
